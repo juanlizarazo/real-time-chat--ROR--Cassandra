a337b542885858b9e9bce9cdd8278a79b289c8b6
juanlizarazo <gabriellg4@gmail.com>
2014-01-13 21:04:25 -0700

    Chat rails app, basic files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmChat::Application.config.secret_key_base = '248f9bf650662af483ce500dc34f3f1ef1ccf0e77d889d2a1578fef97d3b86aae92a094ee73fef322f8fa4614a6cc5310e969a2ba4e80e35ab470660478537e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AmChat::Application.config.session_store :cookie_store, key: '_am_chat_session'
