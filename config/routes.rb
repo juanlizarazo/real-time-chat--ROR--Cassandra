7ac2391c1e0e3be85e9c7bb738a3c8a882473169
jane <val@val.(none)>
2014-01-23 23:14:49 -0700

    Controller made for the user choice/login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 AmChat::Application.routes.draw do
+  get "users/open"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
