b9075ae2d9ff13385f7b29c22adbec2a720e5b48
Your Name <devanallen@artistsmecca.com>
2014-02-02 00:56:28 -0700

    changed the function to render_profile_pic and put image_tag in function addresses issue #23

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  helper_method :load_profile_pic
+  helper_method :render_profile_pic
   
-  def load_profile_pic (user_id)
-    ("profile_pics/" + user_id.profile_pic)
+  def render_profile_pic (user)
+    image_tag("profile_pics/" + user.profile_pic)
   end
 end
