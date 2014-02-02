0054aaf228bd80f3941fe08ec10a9ad562532eb0
Your Name <devanallen@artistsmecca.com>
2014-02-02 00:28:05 -0700

    creates the function to load_profile_pic of the given user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  helper_method :load_profile_pic
+  
+  def load_profile_pic (user_id)
+    ("profile_pics/" + user_id.profile_pic)
+  end
 end
